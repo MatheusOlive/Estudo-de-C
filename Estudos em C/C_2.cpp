097fd3b169fb8c3bba961b87b9c5a66d62a7bf2a
Matheus Oliveira de Souza <matheusolive219@gmail.com>
2021-03-09 22:31:11 -0300

    Continuacao est.
    comeco variaveis em C

---
+++ Estudos em C/C_2.cpp
@@ -0,0 +1,30 @@
+//Curso Udemy - 
+/*
+Aprenda e Domine Uma Das Mais Populares Linguagens de programação: “A Linguagem C”
+Prof Antonio Henrique Reis
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+ // seção 4 variáveis
+ 
+ int numero;
+ 
+ numero = 39;
+ 
+ printf("O valor da variavel numero: %d\n", numero);
+ 
+ numero = numero + 10;
+ 
+ printf("O novo valor da variavel numero: %d\n", numero);
+ 
+ 
+ 
+ return 0 ;
+
+
+}
+
