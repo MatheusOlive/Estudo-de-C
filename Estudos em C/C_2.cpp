85b26e6ffcb606da2fa0c9151ba75231b75f38af
Matheus Oliveira de Souza <matheusolive219@gmail.com>
2021-03-10 22:39:52 -0300

    Novos estudos de C
    3º seção de Váriaveis.

--- Estudos em C/C_2.cpp
+++ Estudos em C/C_2.cpp
@@ -11,7 +11,11 @@ int main ()
 {
  // seção 4 variáveis
  
- int numero;
+
+ /*
+ int numero; //declaração de váriaveis.
+ float nota1, nota2, nota3, media;
+ char letra;
  
  numero = 39;
  
@@ -19,7 +23,24 @@ int main ()
  
  numero = numero + 10;
  
- printf("O novo valor da variavel numero: %d\n", numero);
+ printf("O novo valor da variavel numero: %d\n", numero); 
+ */
+ 
+ //iniciando váriaveis
+ 
+    int numero = 46;
+    float valor = 8.37;
+    char letra = 'X';
+    int valor1, valor2, valor3;
+ 
+    valor1 = valor2 = valor3 = 0;
+ 
+    printf("Variavel numero...: %d \n", numero);
+    printf("Variavel numero...: %f \n", valor);
+    printf("Variavel numero...: %10.2f \n", valor); // 10 numeros de espaço antes de começar e dois de limite aós o ponto 
+    printf("Variavel numero...: %.2f \n", valor); // teste de float .2 somente dois numeros apareceram após o ponto.
+    printf("Variavel numero...: %c \n", letra); //char
+    printf("%d %d %d \n", valor1, valor2, valor3);
  
  
  
