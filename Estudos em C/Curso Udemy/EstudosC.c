faae2ce768c9f0fabff03ae51eac789b3ee69304
Matheus Oliveira de Souza <matheusolive219@gmail.com>
2021-03-16 18:34:47 -0300

    mudanças de texto

--- Estudos em C/Curso Udemy/EstudosC.c
+++ Estudos em C/Curso Udemy/EstudosC.c
@@ -10,21 +10,18 @@ Prof Antonio Henrique Reis
 int main ()
 {
 	
-	//printf("Brasil\n");
-	
-	//Exercicio 1
-	//printf("Matheus\n");
-	//printf("Maria\n");
-	//printf("Nicacia\n");
-	
-	//printf ("Fizemos %d programas\n", 3); // numero
-	//printf ("Letras %c apresentadas\n", 'X'); // char
-	//printf ("Frase: %s\n","frase escrita "); // string
-	//printf ("Um numero qualquer %d \n", 347);	// numero novamente
+	printf("Brasil\n");
+	Exercicio 1
+	printf("Matheus\n");
+	printf("Maria\n");
+	printf("Nicacia\n");
+	printf ("Fizemos %d programas\n", 3); // numero
+	printf ("Letras %c apresentadas\n", 'X'); // char
+	printf ("Frase: %s\n","frase escrita "); // string
+	printf ("Um numero qualquer %d \n", 347);	// numero novamente
 	
 	
 	//Exercicio 2
-	/*
 	printf("Meu\n");
 	printf("\n");
 	printf("\tPrimeiro\n", 1);
@@ -32,26 +29,15 @@ int main ()
 	printf("\t\tPrograma\n", 3);
 	printf("\n");
 	printf("\t\t\tEm C\n", 3);
-	*/
+	
 	
 	//Exercicio 3
-	/*
 	printf("\\Teste de Barra\\\n");
-	*/
+	
 	
 	// Execício 4
-	/*
+	
 	printf("Meu \"querido amigo\" mudou-se!\n");
-	*/
-	
-	
-	
-	
-	
-	
-	
-	
-		
 		
 	system("Pause");
 	return 0;
