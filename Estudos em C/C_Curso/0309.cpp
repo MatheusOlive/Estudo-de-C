c10eb364a1ab8aa43988fc36c97fe9ef12ffb3de
Matheus Oliveira de Souza <47752687+MatheusOlive@users.noreply.github.com>
2021-09-05 21:11:54 -0300

    Update 0309.cpp

--- Estudos em C/C_Curso/0309.cpp
+++ Estudos em C/C_Curso/0309.cpp
@@ -10,7 +10,7 @@ int main ()
 	int contar, resultado, numero;
 	
 	contar = 0; // inicializacao da variavel contadora
-	while(contar < 5) // teste de repetição
+	while(contar < 5) // teste de repetiÃ§Ã£o
 	{
 		printf("Digite um numero: ");
 		scanf("%d", &numero);
@@ -34,24 +34,141 @@ int main ()
 	printf("Quantidade de numeros digitados: %d \n", qtdDigitada);
 	*/
 	
-	//errado exercicio 30 
-	int numero = 1, numPos, numNeg;
+	/*
+	int numero = 99, numPos = 0, numNeg = 0;
 	
-	while( numero == 0 )
+	while( numero != 0 )
 	{
 		printf("Digite um numero: ");
 		scanf("%d", &numero);	
 		if ( numero > 0 )
-		numPos++;
+		numPos = numPos + numero;
 		else
-		numNeg--;
+		numNeg = numNeg + numero;
 	}
 	
 	printf("Numeros Positivos: %d \n", numPos);
 	printf("Numeros Negativos: %d \n", numNeg);
+	*/
+	
+	/*
+	int numero, soma = 0 , qtdLido = 0;
+	float media ;
+	char resposta = 's';
+	
+	while(resposta == 's')
+	{
+		printf("Digite um numero: ");
+		scanf("%d", & numero);
+		
+		soma += numero ; //soma = soma + numero; -= *= /= %=
+		qtdLido++;
+		
+		printf("Deseja digitar outro numero: (s/n)? ");
+		resposta = _getche();
+		printf("\n\n");
+	}
+	
+	media = ((float)soma / (float)qtdLido); // cast forcando o resultado a ser float e economizando memoria 
+	printf("A media dos  numeros lidos foi: %.3f \n", media);	
+	*/
+	
+	
+	/*
+	float numero, numero2, numero3;
+	float media;
+	char resposta = 's';
+	
+		while(resposta == 's')
+	{
+		printf("Digite um numero.......: ");
+		scanf("%f", &numero);
+		
+		printf("Digite segundo numero..: ");
+		scanf("%f", &numero2);
+		
+		printf("Digite terceiro numero.: ");
+		scanf("%f", &numero3);
+		
+		media = (numero + numero2 + numero3)/3;
+		
+		printf("A media do aluno foi.: %.2f\ns", media);
+		printf("\n");
+		
+		if (media >= 6)
+		{
+			printf("Aluno Aprovado");
+		}
+		else
+		{
+			printf("aluno Reprovado");			
+		}
+		
+		printf("\n");
+		
+		printf("Deseja digitar nota de outro aluno: (s/n)? ");
+		resposta = _getche();
+		printf("\n\n");
+	}	
+	*/
 	
 	
 	
+		int codigo, divisor1 = 0, divisor2 = 0, divisor3 = 0, idadeSol = 0, idadeCas = 0, idadeDiv = 0;
+		float dividendo1 = 0, dividendo2 = 0, dividendo3 = 0;
+		char resposta = 's';
+		float mediaSolt, mediaDiv, mediaCas;
+		
+		while(resposta == 's')
+		{
+			system("cls");
+			printf("Digite um codigo de  1 - solteiro / 2 - casado / 3 - Divorciado: ");
+  			scanf("%d", & codigo);
+  			
+  			switch (codigo)
+  			{
+				case 1:					
+				printf("Digite a sua idade: ");
+				scanf("%d", & idadeSol);
+				dividendo1 += idadeSol;
+				divisor1++;		
+				break;
+				
+				case 2:								
+				printf("Digite a sua idade: ");
+				scanf("%d", & idadeCas);
+				dividendo2 += idadeCas;				
+				divisor2++;							
+				break;
+				
+				case 3:				
+				printf("Digite a sua idade: ");
+				scanf("%d", & idadeDiv);
+				dividendo3 += idadeDiv; 				
+				divisor3++;
+				break;
+				
+				default:
+				printf("Foi digitado o codigo errado");
+				break;
+			}
+			
+		printf("\n");	
+		printf("Deseja digitar nota de outra pessoa: (s/n)? ");
+		resposta = _getche();
+		printf("\n\n");	
+				
+		}
+		
+  		mediaSolt = dividendo1 / divisor1;	
+   		mediaCas = dividendo2 / divisor2; 			
+    	mediaDiv = dividendo3 / divisor3;			
+  		
+		printf("A media dos  numeros lidos foi: %.2f \n", mediaSolt);
+		printf("A media dos  numeros lidos foi: %.2f \n", mediaCas);
+		printf("A media dos  numeros lidos foi: %.2f \n", mediaDiv);
+		
+		
 	
 	return 0;
 }
